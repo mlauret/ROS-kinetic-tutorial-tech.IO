ec675abcc39bf03fd4e23f512aab4d0dcc7c8206
Mathieu LAURET <mathieu.lauret30@gmail.com>
2017-10-29 17:03:40 +0100

    Update open_terminal.py

--- ros-project/open_terminal.py
+++ ros-project/open_terminal.py
@@ -1,14 +1,2 @@
 #!/usr/bin/env python
-
-import time
-from subprocess import call
-from subprocess import Popen
-
-Popen(["rosrun", "ros_tutorial", "publisher_tutorial"])
-p = Popen(["rostopic", "echo", "/chatter"])
-
-out, err = p.communicate()
-
-print(out)
-
-time.sleep(1)
+print("TECHIO> terminal")
