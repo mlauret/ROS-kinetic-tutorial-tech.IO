5f7f05045739704b398ac01a89eecd7d27efdef9
Mathieu LAURET <mathieu.lauret30@gmail.com>
2017-11-16 09:11:48 +0100

    Update open_terminal.bash

--- ros-project/open_terminal.bash
+++ ros-project/open_terminal.bash
@@ -1,5 +1,5 @@
 roscore &
-echo "TECHIO> terminal -u ros -i 'source /opt/ros/kinetic/setup.bash && mkdir /home/ros && cd /home/ros'" 2>&1 /dev/null
+echo "TECHIO> terminal -i 'source /opt/ros/kinetic/setup.bash && mkdir /home/ros && cd /home/ros'" 2>&1 /dev/null
 while true
 do
 sleep 1
