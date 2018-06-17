1befdf8efc7c31367df11b9b365aad244193e48f
Mathieu LAURET <mathieu.lauret30@gmail.com>
2018-06-17 20:44:02 +0200

    Update install.bash

--- ros-project/install.bash
+++ ros-project/install.bash
@@ -3,13 +3,16 @@
 # Install catkin tools for building and curl for npm
 apt -qq update && apt -y -qq install python-catkin-tools curl && chmod 777 *.bash
 
-apt-get update -yq && apt-get upgrade -yq && \
 apt-get install -yq curl git nano
 curl -sL https://deb.nodesource.com/setup_8.x | bash - && \
 apt-get install -yq nodejs build-essential
+
 
 
 #get nodejs turtlesim
 git clone https://github.com/mlauret/web-turtlesim
 cd web-turtlesim
 /usr/lib/node_modules/npm/bin/npm-cli.js install
+
+#Install turtlesim for messages and services
+apt -y -qq install ros-kinetic-turtlesim
