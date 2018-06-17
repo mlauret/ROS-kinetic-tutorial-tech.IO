9512abf93c3ce754e5152381fe57bb053dee9719
Mathieu LAURET <mathieu.lauret30@gmail.com>
2018-06-17 20:04:01 +0200

    Update install.bash

--- ros-project/install.bash
+++ ros-project/install.bash
@@ -3,19 +3,10 @@
 # Install catkin tools for building and curl for npm
 apt -qq update && apt -y -qq install python-catkin-tools curl && chmod 777 *.bash
 
-
-# update and install all required packages (no sudo required as root)
-# https://gist.github.com/isaacs/579814#file-only-git-all-the-way-sh
 apt-get update -yq && apt-get upgrade -yq && \
-apt-get install -yq g++ libssl-dev apache2-utils curl git python make nano
-
-# install latest Node.js and npm
-# https://gist.github.com/isaacs/579814#file-node-and-npm-in-30-seconds-sh
-mkdir ~/node-latest-install && cd $_ && \
-curl http://nodejs.org/dist/node-latest.tar.gz | tar xz --strip-components=1 && \
-./configure && \
-make install && \ # takes a few minutes to build...
-curl -k -O -L https://www.npmjs.org/install.sh | sh
+apt-get install -yq curl git nano
+curl -sL https://deb.nodesource.com/setup | sudo bash - && \
+apt-get install -yq nodejs build-essential
 
 
 #get nodejs turtlesim
