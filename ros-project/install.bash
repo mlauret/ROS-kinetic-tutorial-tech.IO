f80f0e4367b970589eaed22f1c6d156eb7d67465
Mathieu LAURET <mathieu.lauret30@gmail.com>
2018-06-17 20:20:33 +0200

    Update install.bash

--- ros-project/install.bash
+++ ros-project/install.bash
@@ -12,4 +12,4 @@ apt-get install -yq nodejs build-essential
 #get nodejs turtlesim
 git clone https://github.com/mlauret/web-turtlesim
 cd web-turtlesim
-/usr/local/bin/npm install
+ls /usr/local/bin/
