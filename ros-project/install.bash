963e48e123905726c8c9c9939ca7044b7a8b1f37
Mathieu LAURET <mathieu.lauret30@gmail.com>
2018-06-17 14:12:30 +0200

    Update install.bash

--- ros-project/install.bash
+++ ros-project/install.bash
@@ -5,24 +5,9 @@ apt -qq update && apt -y -qq install python-catkin-tools && chmod 777 *.bash
 
 
 # Install Docker CE for running Nodejs TurlteSim
-
-apt -y -qq install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    software-properties-common
-    
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-
-add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-apt -qq update
-apt -y -qq install docker-ce
-
+curl -fsSL get.docker.com -o get-docker.sh
+sh get-docker.sh
 
 
 # Get Nodejs Turtlesim from dockerhub
-
 docker pull mathieula/web-turtlesim
