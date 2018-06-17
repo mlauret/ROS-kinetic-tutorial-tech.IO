390c2dd834a7bea64a634550bd643158fc402bb8
Mathieu LAURET <mathieu.lauret30@gmail.com>
2018-06-17 14:21:26 +0200

    Update install.bash

--- ros-project/install.bash
+++ ros-project/install.bash
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-# Install catkin tools for building and curl for docker
+# Install catkin tools for building and curl for npm
 apt -qq update && apt -y -qq install python-catkin-tools curl && chmod 777 *.bash
 
 
-# Install Docker CE for running Nodejs TurlteSim
-curl -fsSL get.docker.com -o get-docker.sh
-sh get-docker.sh
-eval "$(docker-machine env default)"
+#install nvm
+curl --silent -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.2/install.sh | bash
+
+#Install nodejs and npm
+source $NVM_DIR/nvm.sh \
+    && nvm install 6 \
+    && nvm alias default 6 \
+    && nvm use default
 
 
-# Get Nodejs Turtlesim from dockerhub
-docker pull mathieula/web-turtlesim
+#get nodejs turtlesim
+git clone https://github.com/mlauret/web-turtlesim
+cd web-turtlesim
+npm install
