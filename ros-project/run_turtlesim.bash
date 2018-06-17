c80ea4f038bfb3ce67bdad70dc29f2b91e1159d9
Mathieu LAURET <mathieu.lauret30@gmail.com>
2018-06-17 20:45:49 +0200

    Create run_turtlesim.bash

---
+++ ros-project/run_turtlesim.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+roscore &
+mkdir catkin_workspace
+cd catkin_workspace
+mkdir src
+cd src
+catkin_init_workspace .
+cd ..
+cp -r ../publisher_tutorial/ src/.
+catkin_make
+source devel/setup.bash
+cd web-turtlesim
+npm start&
+echo "TECHIO> open -p 3000"
+rosrun publisher_tutorial publisher_tutorial &
+rostopic echo /chatter
