aebba50ce066c4c90c287c8d686ec02ab12ac1f5
Mathieu LAURET <mathieu.lauret30@gmail.com>
2018-06-17 21:06:07 +0200

    Update run_turtlesim.bash

--- ros-project/run_turtlesim.bash
+++ ros-project/run_turtlesim.bash
@@ -11,6 +11,6 @@ catkin_make
 source devel/setup.bash
 cd ../web-turtlesim
 npm start&
+sleep 1
+rosrun publisher_tutorial publisher_tutorial&
 echo "TECHIO> open -p 3000"
-rosrun publisher_tutorial publisher_tutorial &
-rostopic echo /chatter
